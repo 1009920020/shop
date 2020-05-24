e39198f2239086b686735c96dfe267a6829d15a0
1009920020 <56160567+1009920020@users.noreply.github.com>
2020-05-24 21:27:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
-//设置字体
 header("content-type:text/html;charset=utf-8");
-
-define('HEA_URL', 'http://localhost/index.php');
-
-//设置系统的模式
-define('APP_DEBUG',true);   //开发
-//define('APP_DEBUG',false);   //生产
-
+define('APP_DEBUG',true);
 define('CSS_URL1', '/Public/Home/css/');
 define('IMG_URL1', '/Public/Home/images/');
-
 define('ADMIN_CSS_URL1','/Public/Admin/css/');
 define('ADMIN_FONTS_URL1','/Public/Admin/fonts/');
 define('ADMIN_JS_URL1','/Public/Admin/js/');
-
 include "./ThinkPHP/ThinkPHP.php";
